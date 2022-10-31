c6bbf09032e45de0fb9f1dac599316f16cd8ab71
Milos Jeremic <Milos.Jeremic@bdkadvokati.com>
2022-10-31 17:13:15 +0100

    promenjeno ime klase card

--- components/Card/NameCard.vue
+++ components/Card/NameCard.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="card">
-     <h3>{{ namesSelected }}</h3>
+     <h3>{{ 6f5cf78b6ce9fc46ea067b6a803100191021337d }}</h3>
+     <h3>{{ 6f5cf78b6ce9fc46ea067b6a803100191021337d }}</h3>
     <p>x</p>
   </div>
 </template>
@@ -9,6 +10,7 @@
 
 interface NameProps {
     name: string;
+
  }  
 
 const props = defineProps<NameProps>();
