f01fba6c21b62a766d866ae9f55eae86e28a748d
Milos Jeremic <Milos.Jeremic@bdkadvokati.com>
2022-11-01 11:33:28 +0100

    formating

--- components/Card/NameCard.vue
+++ components/Card/NameCard.vue
@@ -1,15 +1,13 @@
 <template>
   <div class="card">
-     <h3>{{ namesSelected }}</h3>
+    <h3>{{ namesSelected }}</h3>
     <p>x</p>
   </div>
-</template>
-
+</template>git 
 <scrypt setup lang="ts">
 
 interface NameProps {
     name: string;
-    
  }  
 
 const props = defineProps<NameProps>();
